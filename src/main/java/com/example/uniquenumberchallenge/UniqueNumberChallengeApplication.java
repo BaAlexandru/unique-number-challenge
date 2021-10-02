59d2273025336bd2964b2979eaafa67317641125
alexandru <a.balaban@reply.de>
2021-10-02 22:32:28 +0200

    Setting the project

---
+++ src/main/java/com/example/uniquenumberchallenge/UniqueNumberChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.uniquenumberchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniqueNumberChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniqueNumberChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uniquenumberchallenge/UniqueNumberChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uniquenumberchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniqueNumberChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
